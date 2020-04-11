6ad8329c5909531e2d4f86144447414ec7716f47
test <793021792@qq.com>
2020-04-11 11:31:35 +0800

    test chuancan

--- vue-cli-rooter/src/App.vue
+++ vue-cli-rooter/src/App.vue
@@ -9,18 +9,46 @@
       </p>
       <el-button>el-button</el-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to yout vue.js App" @click="onClick"/>
+    <HelloWorld msg="Welcome to yout vue.js App" ref="hw2">
+      <template>
+        abc
+      </template>
+      <template v-slot:component>
+        content..............
+      </template>
+    </HelloWorld>
+    <helloword2/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import helloword2 from './components/helloword2.vue'
 
 export default {
   name: 'app',
+  provide(){
+    //隔代传参，类似于data
+    return {
+      bar:"barrrrrrrrrrrrrrr",
+      app:this
+    }
+  },
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    helloword2
+  },
+  mounted(){
+    this.$refs.hw2.xx="balbalababaa"
+  },
+  methods: {
+    onClick() {
+      console.log("click me");
+
+      
+    }
+  },
 }
 </script>
 
---
+++ vue-cli-rooter/src/components/helloword2.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button>say hi 2</button>
+    </div>
+</template>
+
+<script>
+    export default {
+    mounted(){
+      //监听事件
+      this.$parent.$on('foo',msg=>{
+        console.log(msg);
+      })
+    },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
